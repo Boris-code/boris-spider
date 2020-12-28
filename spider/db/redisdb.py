3d700a0744a07fa57b571e9d8d9e75e506c44865
刘博-熵简 <liubo@entropyreduce.com>
2020-12-28 12:25:22 +0800

    redisdb 使用单例模式

--- spider/db/redisdb.py
+++ spider/db/redisdb.py
@@ -15,6 +15,18 @@ from redis.sentinel import Sentinel
 from rediscluster import StrictRedisCluster
 
 
+class Singleton(object):
+    def __init__(self, cls):
+        self._cls = cls
+        self._instance = {}
+
+    def __call__(self):
+        if self._cls not in self._instance:
+            self._instance[self._cls] = self._cls()
+        return self._instance[self._cls]
+
+
+@Singleton
 class RedisDB:
     def __init__(
         self,
